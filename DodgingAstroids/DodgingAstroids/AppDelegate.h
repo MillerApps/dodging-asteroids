263d0daa8d038bed629f89ba14ef82e57ce7e7e6
Tyler Miller <tyemiller4@gmail.com>
2015-02-05 15:31:56 -0500

    Initial Filies

---
+++ DodgingAstroids/DodgingAstroids/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DodgingAstroids
+//
+//  Created by Tyler Miller on 2/5/15.
+//  Copyright (c) 2015 Tyler Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DodgingAstroids/DodgingAstroids/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  DodgingAstroids
+//
+
+//  Copyright (c) 2015 Tyler Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
