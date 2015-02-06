e72ed629e4f0e25e23276b3572236d939b777f8a
Tyler Miller <tyemiller4@gmail.com>
2015-02-05 20:44:04 -0500

    done with game concept/moving to sound effects

--- DodgingAstroids/DodgingAstroids/GameScene.m
+++ DodgingAstroids/DodgingAstroids/GameScene.m
@@ -14,7 +14,29 @@
     //add spaceship to scene
     SKSpriteNode *spaceShip = [SKSpriteNode spriteNodeWithImageNamed:@"playerShip"];
     spaceShip.position = CGPointMake(self.size.width/2, 100);
+    
     [self addChild:spaceShip];
+    
+    //add spaceship exhaust
+    
+    SKEmitterNode *exhaust = [SKEmitterNode nodeWithFileNamed:@"ExhaustParticle.sks"];
+    exhaust.position = CGPointMake(0, -35);
+    [spaceShip addChild:exhaust];
+    
+    
+    
+}
+
+-(void)addAstroids {
+    // astroids Will be added to the scene at random positions in the fully functional Game
+    //add astroids to scene
+    SKSpriteNode *astroid = [SKSpriteNode spriteNodeWithImageNamed:@"meteorBrown_big1"];
+    astroid.position = CGPointMake(50, self.size.height/2);
+    [self addChild:astroid];
+    
+    SKSpriteNode *astroidTwo = [SKSpriteNode spriteNodeWithImageNamed:@"meteorBrown_big3"];
+    astroidTwo.position = CGPointMake(320, self.size.height - 60);
+    [self addChild:astroidTwo];
 }
 
 -(void)didMoveToView:(SKView *)view {
@@ -24,7 +46,11 @@
     background.size = self.size;
     [self addChild:background];
     
+    //set physicsbody for scene
+    self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    
     [self addPlayerShip];
+    [self addAstroids];
 }
 
 
